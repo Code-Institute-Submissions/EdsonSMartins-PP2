2f67d785853b2d20c404577820f0d0733b93beeb
EdsonSMartins <smartins.edson@gmail.com>
2023-06-02 19:52:33 +0000

    Add questions and answers to quiz js file

--- assets/JS/quiz.js
+++ assets/JS/quiz.js
@@ -1,82 +1,82 @@
 const questions = [
     {
-        text: "",
-        options: ["", "", ""],
-        answer: ""
+        text: "For at least how many years must Irish whiskey be wood-aged?",
+        options: ["3 years", "7 years", "10 years"],
+        answer: "3 years"
     },
     {
-        text: "",
-        options: ["", "", ""],
-        answer: ""
+        text: "Which of the following tool is used by a bartender to measure amounts of liquid precisely?",
+        options: ["A shot", "A jigger", "A shaker"],
+        answer: "A jigger"
     },
     {
-        text: "",
-        options: ["", "", ""],
-        answer: ""
+        text: "Which liquor is the main ingredient in the Bloody Mary?",
+        options: ["Rum", "Tequila", "Vodka"],
+        answer: "Vodka"
     },
     {
-        text: "",
-        options: ["", "", ""],
-        answer: ""
+        text: "The Long Island Iced Tea consists of gin, triple sec, light rum, tequila, vodka, and a splash of what on top?",
+        options: ["Lemon juice", "Honey", "Cola"],
+        answer: "Cola"
     },
     {
-        text: "",
-        options: ["", "", ""],
-        answer: "fad"
+        text: "Which bartending technique should we use for any cocktail recipe with mixers?",
+        options: ["Shaking", "Stirring", "Straining"],
+        answer: "Shaking"
     },
     {
-        text: "",
-        options: ["", "", ""],
-        answer: ""
+        text: "Which of the following cocktails should be stirred well to combine the added ingredients?",
+        options: ["Manhattan", "New Orleans Fizz", "Mai Tai"],
+        answer: "Manhattan"
     },
     {
-        text: "",
-        options: ["", "", ""],
-        answer: ""
+        text: "Which of the following liquors is distilled from fruits, especially grapes?",
+        options: ["Vodka", "Gin", "Brandy"],
+        answer: "Brandy"
     },
     {
-        text: "",
-        options: [""],
-        answer: ""
+        text: "Which of the following cocktails can be ordered dirty or dry?",
+        options: ["Old Fashioned", "Negroni", "Martini"],
+        answer: "Martini"
     },
     {
-        text: "",
-        options: ["", "", ""],
-        answer: ""
+        text: "Which ingredient in the vodka-based drink Cosmopolitan gives it the pink colour?",
+        options: ["Cranberry juice", "Cherry juice", "Strawberry juice"],
+        answer: "Cranberry juice"
     },
     {
-        text: "",
-        options: ["", "", ""],
-        answer: ""
+        text: "Which tool can be used by a bartender to crush fruits?",
+        options: ["Jigger", "Muddler", "Mister"],
+        answer: "Muddler"
     },
     {
-        text: "",
-        options: ["", "", ""],
-        answer: ""
+        text: "The typical taste of the liqueur Kahlua is quite similar to which popular drink?",
+        options: ["Chocolate", "Coffee", "Tea"],
+        answer: "Coffee"
     },
     {
-        text: "",
-        options: ["", "", ""],
-        answer: ""
+        text: "Which of the following is NOT typically found in an Old Fashioned?",
+        options: ["Vermouth", "Bitters", "Sugar"],
+        answer: "Vermouth"
     },
     {
-        text: "",
-        options: ["", "", ""],
-        answer: ""
+        text: "Which three classic cocktails would you make using a julep strainer?",
+        options: ["Manhattan, Martini, Margarita", "Martini, Vieux Carré, Negroni", "Martini, Old Fashioned, Mint Julep"],
+        answer: "Martini, Vieux Carré, Negroni"
     },
     {
-        text: "",
-        options: ["", "", ""],
-        answer: ""
+        text: "What is the purpose of a dry shake?",
+        options: ["To emulsify egg whites", "To aerate fresh lime juice", "To emulsify egg yolks "],
+        answer: "To emulsify egg whites"
     },
     {
-        text: "",
-        options: ["", "", ""],
-        answer: ""
+        text: "Which of the following is NOT typically found in an Aperol Spritz?",
+        options: ["Simple Syrup", "Soda Water", "Prosecco"],
+        answer: "Prosecco"
     },
     {
-        text: "",
-        options: ["", "", ""],
-        answer: ""
+        text: "What were the main ingredients in Poitín?",
+        options: ["Potato", "Carrot", "Beetroot"],
+        answer: "Potato"
     },
 ];
