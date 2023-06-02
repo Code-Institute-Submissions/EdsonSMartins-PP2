77404f4c35da7e851b45c07fb69662efa0e71aa9
EdsonSMartins <smartins.edson@gmail.com>
2023-06-02 23:46:14 +0000

    Add main structure to quiz on html and add style.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,37 +2,33 @@
 // variables
 
 const myModal = new bootstrap.Modal('#modal');
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
 
-// define questionList 
-let questionList = null;
+const gameSection = document.querySelector(".game-play");
+const resultSection = document.querySelector(".result");
 
-document.addEventListener("DOMContentLoaded", function () {
-	getJsonThenLoad();
-});
+//main game variables
+const questionElement = document.getElementById('questions');
+const allAnswers = document.getElementById('answer-buttons');
+const nextButton = document.querySelector('.next-btn');
 
-const getJsonThenLoad = function () {
-	fetch("./assets/js/quiz.js")
-		.then(file => {
-			return file.json();
-		})
-		.then(json => {
-			questionList = json;
-		})
-		.then(() => buildQuiz());
-};
+function startQuiz(){
+	currentQuestionIndex = 0;
+	score = 0;
+	nextButton.innerHTML = "Next";
+	showQuestion();
+}
 
-function buildQuiz(){}
+function showQuestion(){
+	let currentQuestion = questions[currentQuestionIndex];
+	let questionNo = currentQuestionIndex + 1;
+	questionElement.innerHTML = questionNo + "." + currentQuestion.question;
 
-function showResults(){}
+	currentQuestion.answers.forEach(answer => {
+		const button = document.createElement("button");
+		button.innerHTML = answer.text;
+		button.classList.add("btn");
+		answerButton.appendChild(button);
+	})
+}
 
-// display quiz right away
-buildQuiz();
-
-// on submit, show results
-submitButton.addEventListener('click', showResults);
-
-
-
+startQuiz();
