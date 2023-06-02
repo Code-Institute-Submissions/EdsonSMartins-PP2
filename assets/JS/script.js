3039fdaac60d3a6737c13c78e0d2446e921be3d6
EdsonSMartins <smartins.edson@gmail.com>
2023-06-02 10:27:34 +0000

    Edit instructions content

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,10 @@
-const myModal = bootstrap.Modal('#modalH1');
+// to open model when page load
+
+const myModal = new bootstrap.Modal('#modal');
 
 window.addEventListener('DOMContentLoad', () => {
   myModal.show()
 })
+
+// to close modal
+document
