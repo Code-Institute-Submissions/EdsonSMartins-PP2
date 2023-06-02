b0a2e9f1fda40b77a968f9d572a78d0fbb1e0df9
EdsonSMartins <smartins.edson@gmail.com>
2023-06-02 13:25:04 +0000

    Add jquery to launch modal when page load

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,8 @@
-// to open model when page load
+
 
 const myModal = new bootstrap.Modal('#modal');
 
-window.addEventListener('DOMContentLoad', () => {
-  myModal.show()
-})
 
-// to close modal
-document
+
+
+
